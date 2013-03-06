1e74d7cf9473b3f7b638a59fdedd3d5860841c0b
msh986 <matthewhlavacek2014@u.northwestern.edu>
2013-03-06 16:00:20 -0600

    tons of new updates

---
+++ table.h
@@ -0,0 +1,58 @@
+#ifndef _table
+#define _table
+
+
+#include <iostream>
+
+using namespace std;
+
+#if defined(GENERIC)
+class Table {
+  // Students should write this class
+ public:
+  ostream & Print(ostream &os) const;
+};
+#endif
+
+
+#if defined(LINKSTATE)
+#include <deque>
+
+class Table {
+	private:
+	map<int,double> costTable;
+	map<int, int> nextHop;
+	unsigned id;
+	vector<unsigned> nodes;
+	vector<Link*> links;
+	void dijkstra();
+  // Students should write this class
+ public:
+  ostream & Print(ostream &os) const;
+  unsigned GetNext(const unsigned goal);
+  bool LinkUpdate(const unsigned src, const unsigned dest, double l, double ts);
+  Table *get_routing_table() const;
+};
+#endif
+
+#if defined(DISTANCEVECTOR)
+
+#include <deque>
+
+class Table {
+ private:
+ 	map<unsigned, map<unsigned, double>> dist;
+ 	map<unsigned, unsigned> nextHop;
+ 	Node* id;
+ public:
+   ostream & Print(ostream &os) const;
+   unsigned	GetNext(unsigned end);
+   map<unsigned,double> GetRow();
+   bool RowUpdate(const unsigned src, const map<unsigned,double> toUpdate);
+   bool SelfUpdate(const Link *l);
+};
+#endif
+
+inline ostream & operator<<(ostream &os, const Table &t) { return t.Print(os);}
+
+#endif
